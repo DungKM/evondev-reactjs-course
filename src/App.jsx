de79f8d94f21df3e87a47dca60941ceb202a9bbe
DungKM <hoanganhdung13122000@gmail.com>
2025-03-24 22:01:53 +0700

    Feat: refactor App component to include Feature component and update content ✨ex10

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-
-const App = () => {
-  const name = "React";
-  function formatName(firstName, lastName) {
-    return `${firstName} ${lastName}`;
-  }
-  // return (
-  //   <div className="App">
-  //     <h1> Hello {name == "React" ? "Dung" : "Lan"}</h1>
-  //     <h2>{formatName("Hoang Anh", "Dung")}</h2>
-  //   </div>
-  // );
+function Feature() {
   return (
     <div className="feature">
       <img
@@ -17,13 +6,23 @@ const App = () => {
         alt="random"
         className="feature-image"
       />
-      <h3>Hello World</h3>
+      <h3>Title2</h3>
       <p className="feature-desc">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
         quibusdam, voluptatem, nemo, quia quae quod odio laborum voluptas
         doloremque nesciunt sequi. Nihil, doloremque. Quisquam quibus
         voluptatem, voluptates quas quidem.
       </p>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <Feature></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
     </div>
   );
 };
