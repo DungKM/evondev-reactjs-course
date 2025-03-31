88b9899d514dfec9a118ddcc29de336cd7a2d809
DungKM <hoanganhdung13122000@gmail.com>
2025-03-31 22:41:23 +0700

    Feat: implement dynamic rendering of YoutubeItem components using YoutubeData array

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,45 @@
 import "./App.css";
-
-function Feature() {
-  return (
-    <div className="feature">
-      <img
-        src="https://picsum.photos/200/300"
-        alt="random"
-        className="feature-image"
-      />
-      <h3>Title2</h3>
-      <p className="feature-desc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        quibusdam, voluptatem, nemo, quia quae quod odio laborum voluptas
-        doloremque nesciunt sequi. Nihil, doloremque. Quisquam quibus
-        voluptatem, voluptates quas quidem.
-      </p>
-    </div>
-  );
-}
+import { YoutubeData } from "./YoutubeData";
 
 // parent component
 const App = () => {
+  const numbers = [1, 2, 3, 4, 5];
+  const doubleNumbers = numbers.map((number) => number * 2);
+  console.log(YoutubeData);
   return (
     <div className="youtube-list">
       {/* children components  */}
-      <YoutubeItem
-        image="https://picsum.photos/200/200"
-        title="Learning React"
-        channel="Codevolution"
-      ></YoutubeItem>
-      <YoutubeItem
-        image="https://picsum.photos/200/200"
-        title="Learning Angular"
-        channel="Anh Dung"
-      ></YoutubeItem>
-      <YoutubeItem image="https://picsum.photos/200/200"></YoutubeItem>
-      <YoutubeItem image="https://picsum.photos/200/200"></YoutubeItem>
+      {YoutubeData.map((item, index) => (
+        <YoutubeItem
+          key={item.id}
+          image={item.image}
+          title={item.title}
+          channel={item.channel}
+          avatar={item.avatar}
+        ></YoutubeItem>
+      ))}
     </div>
   );
 };
 
 // Props -> properties
-
 function YoutubeItem(props) {
   return (
     <div className="youtube-item">
       <div
         className="youtube-image"
-        // style={{
-        //   height: "200px",
-        // }}
       >
         <img
           src={props.image}
           alt="random"
-          // style={{ display: "block", maxWidth: "100%" }}
         />
       </div>
       <div className="youtube-footer">
-          <img src="https://picsum.photos/200/200" alt="" className="youtube-avatar" />
+        <img
+          src={props.avatar || "https://picsum.photos/200/200"}
+          alt=""
+          className="youtube-avatar"
+        />
         <div className="youtube-infor">
           <h3 className="youtube-title">
             {props.title || "This is example of title"}
---
+++ src/YoutubeData.js
@@ -0,0 +1,23 @@
+export const YoutubeData = [
+  {
+    id: 1,
+    title: "React JS Tutorial for Beginners 1",
+    channel: "PhanVanLuan",
+    image: "https://picsum.photos/200/200",
+    avatar: "https://picsum.photos/200/200",
+  },
+  {
+    id: 2,
+    title: "React JS Tutorial for Beginners 2",
+    channel: "Codevolution",
+    image: "https://picsum.photos/200/200",
+    avatar: "https://picsum.photos/200/200",
+  },
+  {
+    id: 3,
+    title: "React JS Tutorial for Beginners 3",
+    channel: "HoangAnhDung",
+    image: "https://picsum.photos/200/200",
+    avatar: "https://picsum.photos/200/200",
+  },
+];
