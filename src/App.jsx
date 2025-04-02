abd67231f9fbd956aa44f9be8980c8dfcd54074b
DungKM <hoanganhdung13122000@gmail.com>
2025-04-02 22:39:51 +0700

    Feat: stateless and stateful document

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ const App = () => {
   const numbers = [1, 2, 3, 4, 5];
   const doubleNumbers = numbers.map((number) => number * 2);
   console.log(YoutubeData);
+  const name = "Phan Van Luan";
   return (
     <div>
-      <YoutubeList />
+      <YoutubeList>
+        {name}
+      </YoutubeList>
     </div>
   );
 };
--- src/components/youtube/YoutubeList.jsx
+++ src/components/youtube/YoutubeList.jsx
@@ -2,9 +2,12 @@ import { YoutubeData } from "../../data";
 import YoutubeItem from "./YoutubeItem";
 
 const YoutubeList = (props) => {
+    console.log(props);
     return (
         <div className="youtube-list">
             {/* children components  */}
+            {props.children}
+            {/* map data  */}
             {YoutubeData.map((item, index) => {
                 let newClass = '';
                 if (index === 1) newClass = "abc";
