a7f8378b0cbc95d3f1811d8d4c570e5fe1a73c28
DungKM <hoanganhdung13122000@gmail.com>
2025-04-02 23:40:58 +0700

    Feat: add toggle functionality to Toggle component with click handler

--- src/components/state/Toggle.jsx
+++ src/components/state/Toggle.jsx
@@ -7,14 +7,18 @@ import "./ToggleStyle.css";
 
 function Toggle() {
     const [on, setOn] = useState(false);
+
+    const handleToggle = () => {
+        setOn(on => !on);
+    }
     return <div>
-        <div className={`toggle ${on ? "active" : ""}`}>
+        <div className={`toggle ${on ? "active" : ""}`} onClick={handleToggle}>
             <div className={`spinner ${on ? "active" : ""}`}></div>
         </div>
-        <div className="toggle-control">
+        {/* <div className="toggle-control">
             <div className="toggle-on" onClick={() => setOn(true)}>On</div>
             <div className="toggle-off" onClick={() => setOn(false)}>OFF</div>
-        </div>
+        </div> */}
     </div >
 }
 export default Toggle;
