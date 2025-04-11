5374cee2dc1e541b43fda7eb6d426aa8391577ef
DungKM <hoanganhdung13122000@gmail.com>
2025-04-11 10:29:37 +0700

    Feat: remove unused Counter and Toggle components from App, add calculateWinner helper function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import Counter from "./components/counter/Counter";
 import Game from "./components/tictactoe/Game";
-import Toggle from "./components/toggle/Toggle";
 
 
 // parent component
 const App = () => {
   return (
     <div>
-      {/* <Toggle></Toggle>
-      <Counter></Counter> */}
       <Game></Game>
     </div>
   );
