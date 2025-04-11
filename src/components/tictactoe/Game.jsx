cf4b551ae08d25fb529729bc41b4d2d120db010c
DungKM <hoanganhdung13122000@gmail.com>
2025-04-11 18:21:26 +0700

    Feat: implement game logic in Game component and enhance styling for better user experience

--- src/components/tictactoe/Game.jsx
+++ src/components/tictactoe/Game.jsx
@@ -6,9 +6,18 @@ const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(board);
-    const handleClick = () => {};
+    const handleClick = (index) => {
+        const BoardCopy = [...board];
+        if (winner || BoardCopy[index]) return;
+        BoardCopy[index] = xIsNext ? "X" : "O";
+        setBoard(BoardCopy);
+        setXisNext(!xIsNext);
+    }
+
     return <div>
         <Board cells={board} onClick={handleClick}></Board>
+        {winner ? <div className="game-winner">Winner: {winner}</div> : <div className="game-next">Next player: {xIsNext ? "X" : "O"}</div>}
+       <button className="game-reset" onClick={() => { setBoard(Array(9).fill(null)); setXisNext(true); }}>Reset</button>
     </div>
 }
 export default Game;
