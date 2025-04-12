8e8b3dcbed55a79a4d4758303d70b4a4b1fb75cd
DungKM <hoanganhdung13122000@gmail.com>
2025-04-12 10:20:33 +0700

    Feat: refactor Game component to use state object for board and player turn management

--- src/components/tictactoe/Game.jsx
+++ src/components/tictactoe/Game.jsx
@@ -3,21 +3,36 @@ import Board from './Board.jsx';
 import "./GameStyles.css";
 import { calculateWinner } from '../../helpers.jsx';
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [xIsNext, setXisNext] = useState(true);
-    const winner = calculateWinner(board);
+    // const [board, setBoard] = useState(Array(9).fill(null));
+    // const [xIsNext, setXisNext] = useState(true);
+    const [state, setState] = useState({
+        board: Array(9).fill(null),
+        xIsNext: true
+    })
+    const winner = calculateWinner(state.board);
     const handleClick = (index) => {
-        const BoardCopy = [...board];
+        const BoardCopy = [...state.board];
         if (winner || BoardCopy[index]) return;
-        BoardCopy[index] = xIsNext ? "X" : "O";
-        setBoard(BoardCopy);
-        setXisNext(!xIsNext);
+        BoardCopy[index] = state.xIsNext ? "X" : "O";
+        // setBoard(BoardCopy);
+        // setXisNext(!xIsNext);
+        setState({
+            ...state,
+            board: BoardCopy,
+            xIsNext: !state.xIsNext
+        })
     }
 
     return <div>
-        <Board cells={board} onClick={handleClick}></Board>
-        {winner ? <div className="game-winner">Winner: {winner}</div> : <div className="game-next">Next player: {xIsNext ? "X" : "O"}</div>}
-       <button className="game-reset" onClick={() => { setBoard(Array(9).fill(null)); setXisNext(true); }}>Reset</button>
+        <Board cells={state.board} onClick={handleClick}></Board>
+        {winner ? <div className="game-winner">Winner: {winner}</div> : <div className="game-next">Next player: {state.xIsNext ? "X" : "O"}</div>}
+        <button className="game-reset" onClick={() => {
+            setState({
+                ...state,
+                board: Array(9).fill(null),
+                xIsNext: true
+            })
+        }}>Reset</button>
     </div>
 }
 export default Game;
